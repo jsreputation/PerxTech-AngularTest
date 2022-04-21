351b338f1450a357c5c9e3299dcb2ca6982c8541
Intel Tech <jsreputation@gmail.com>
2022-04-21 22:50:11 +0800

    feat: search feature to the bookcase page

--- src/app/pages/bookcase/bookcase.component.ts
+++ src/app/pages/bookcase/bookcase.component.ts
@@ -1,21 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject, debounceTime, map, Subject, takeUntil } from 'rxjs';
 import { BookService } from '../../core/services/book.service';
+import { Book } from '../../core/models/book';
 
 @Component({
   selector: 'app-bookcase',
   templateUrl: './bookcase.component.html',
-  styleUrls: ['./bookcase.component.scss']
+  styleUrls: ['./bookcase.component.scss'],
 })
-export class BookcaseComponent implements OnInit {
+export class BookcaseComponent implements OnInit, OnDestroy {
+  data$: BehaviorSubject<Book[]> = new BehaviorSubject<Book[]>([]);
 
-  constructor(
-    private bookService: BookService
-  ) { }
+  searchForm: FormGroup = this.fb.group({
+    keyword: '',
+  });
+
+  private unsubscribeAll: Subject<any> = new Subject();
+
+  constructor(private bookService: BookService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.bookService.getBooks().subscribe(res => {
-      console.log(res);
-    });
+    this.search();
+    this.searchForm
+      .get('keyword')
+      ?.valueChanges.pipe(takeUntil(this.unsubscribeAll), debounceTime(300))
+      .subscribe((keyword) => {
+        this.search();
+      });
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribeAll.next(null);
+    this.unsubscribeAll.complete();
+  }
+
+  private search() {
+    this.bookService
+      .getBooks()
+      .pipe(
+        map((res) => {
+          const keyword = this.searchForm.get('keyword')?.value || '';
+          if (keyword) {
+            return res.data.filter((x) => Boolean(x.attributes.content.includes(keyword)));
+          } else {
+            return res.data;
+          }
+        })
+      )
+      .subscribe((res) => {
+        this.data$.next(res);
+      });
+  }
 }
--- src/app/pages/bookcase/bookcase.module.ts
+++ src/app/pages/bookcase/bookcase.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BookcaseRoutingModule } from './bookcase-routing.module';
 import { BookcaseComponent } from './bookcase.component';
 
-
 @NgModule({
-  declarations: [
-    BookcaseComponent
-  ],
-  imports: [
-    CommonModule,
-    BookcaseRoutingModule
-  ]
+  declarations: [BookcaseComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, BookcaseRoutingModule],
 })
-export class BookcaseModule { }
+export class BookcaseModule {}
