c179bfb3c6e106856ea29d2eb43996fa6b92f0d9
Intel Tech <jsreputation@gmail.com>
2022-04-21 23:41:16 +0800

    feat: show header except home page

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  currentRoute = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    router.events.pipe(filter((event: any) => event instanceof NavigationEnd)).subscribe((event: any) => {
+      if (event.url !== '/') {
+        this.currentRoute = event.url;
+      } else {
+        this.currentRoute = null;
+      }
+      // console.log(this.currentRoute);
+    });
   }
 
+  ngOnInit(): void {}
 }
